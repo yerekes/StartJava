c598ac6859d3180aa74b43dccb108bb2e57a1e77
Yerzat Smagulov <smagulovyerzat@gmail.com>
2022-01-15 02:06:22 +0300

    Yerekes

---
+++ Variable.java
@@ -0,0 +1,12 @@
+public class Variable{
+    public static void main(String[] args) {
+        byte b = 1;
+        short s = 2;
+        int i = 4;
+        long l = 8;
+        float f = 4;
+        double d = 2;
+        char c = 2;
+        boolean b = true;
+    }
+}

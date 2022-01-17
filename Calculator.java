0463aee593273e1e07bcc6f94e6e222a07c2fe3e
Yerzat Smagulov <smagulovyerzat@gmail.com>
2022-01-17 10:45:40 +0300

    irugular

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,9 @@
 public class Calculator{
     public static void main(String[] args) {
-    int num1 = 23;
-    int num2 = 34;
-    char operation = '*';
+    int num1 = 2;
+    int num2 = 3;
+    int chislo = 1;
+    char operation = '^';
     int result;
     if(operation == '+'){
         result = num1 + num2;
@@ -17,7 +18,10 @@ public class Calculator{
             result = num1 / num2;
             System.out.println(result);
         } else if(operation == '^'){
-            result = num1 ^ num2;
+            for(int i=1; i <= num2; i++){
+                chislo = chislo * num1;
+            }
+            result = chislo;
             System.out.println(result);
         } else if(operation == '%'){
             result = num1 % num2;

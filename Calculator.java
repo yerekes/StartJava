708b46562b9f5362c984e3013e6ae17f0ce48970
Yerzat Smagulov <smagulovyerzat@gmail.com>
2022-01-17 15:30:00 +0300

    oire

--- Calculator.java
+++ Calculator.java
@@ -1,29 +1,29 @@
-public class Calculator{
+public class Calculator {
     public static void main(String[] args) {
     int num1 = 2;
     int num2 = 3;
     int chislo = 1;
     char operation = '^';
     int result;
-    if(operation == '+'){
+    if(operation == '+') {
         result = num1 + num2;
         System.out.println(result);
-        } else if(operation == '-'){
+        } else if(operation == '-') {
             result = num1 - num2;
             System.out.println(result);
-        } else if(operation == '*'){
+        } else if(operation == '*') {
             result = num1 * num2;
             System.out.println(result);
-        } else if(operation == '/'){
+        } else if(operation == '/') {
             result = num1 / num2;
             System.out.println(result);
-        } else if(operation == '^'){
-            for(int i=1; i <= num2; i++){
+        } else if(operation == '^') {
+            for(int i=1; i <= num2; i++) {
                 chislo = chislo * num1;
             }
             result = chislo;
             System.out.println(result);
-        } else if(operation == '%'){
+        } else if(operation == '%') {
             result = num1 % num2;
             System.out.println(result);
         }

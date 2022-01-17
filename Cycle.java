b23bf4e8cc9e6a878be6affb196c59bbed9803d1
Yerzat Smagulov <smagulovyerzat@gmail.com>
2022-01-17 15:45:25 +0300

    Comm

--- Cycle.java
+++ Cycle.java
@@ -2,16 +2,18 @@ public class Cycle {
     public static void main(String[] args) {
         int num1 = -6;
         int num2 = 10;
-        for(int i = 0;i<21;i++){
+        for(int i = 0;i < 21;i++) {
             System.out.println(i);
         }
-        while(num1 <= 6){
+
+        while(num1 <= 6) {
             System.out.println(num1);
-            num1 = num1 +2;
+            num1 = num1 + 2;
         }
+
         do{
             int ost = num2 % 2;
-            if(ost == 1){
+            if(ost == 1) {
                 System.out.println(num2);
                 num2 = num2 + 1;
             } else {

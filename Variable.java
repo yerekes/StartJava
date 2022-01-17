6f9a6419c89ae0fa1e924a0cb4786a2c7192e6c9
Yerzat Smagulov <smagulovyerzat@gmail.com>
2022-01-17 12:24:20 +0300

    perekl

--- Variable.java
+++ Variable.java
@@ -1,12 +1,20 @@
-public class Variable{
+public class Variable {
     public static void main(String[] args) {
-        byte b = 1;
-        short s = 2;
-        int i = 4;
-        long l = 8;
-        float f = 4;
-        double d = 2;
-        char c = 2;
-        boolean b = true;
+        byte intelCore = 5;
+        short memory = 4;
+        int graphics = 5000;
+        long graphicsMemory = 1536;
+        float processor = 1.4f;
+        double version = 11.6;
+        char serialNumber = 'C';
+        boolean memoryCard = memory <= 128;
+        System.out.println(intelCore);
+        System.out.println(memory);
+        System.out.println(graphics);
+        System.out.println(graphicsMemory);
+        System.out.println(processor);
+        System.out.println(version);
+        System.out.println(serialNumber);
+        System.out.println(memoryCard);
     }
 }

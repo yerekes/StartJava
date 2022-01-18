4c7d6bb0fd3de6c12542e3291b0f85d0502500c6
Yerzat Smagulov <smagulovyerzat@gmail.com>
2022-01-18 19:07:46 +0300

    Yerzat pereproveril

--- ConditionalStatement.java
+++ ConditionalStatement.java
@@ -1,13 +1,11 @@
 public class ConditionalStatement {
     public static void main(String[] args) {
         int age = 10;
-        double length = 1.60;
-        char male = 'M';
-        char firstLetterName = 'M';
         if(age > 20) {
             System.out.println("Стареешь больше 20и");
         }
         
+        char male = 'M';
         if(male == 'M') {
             System.out.println("Быть мужиком - это круто");
         }
@@ -16,12 +14,14 @@ public class ConditionalStatement {
             System.out.println("Who runs the world- GIRLS");
         }
         
+        double length = 1.60;
         if(length < 1.80) {
             System.out.println("Быть длинным это смотреть с высока на низких");
         } else {
             System.out.println("Быть низким не так уж плохо ");
         }
         
+        char firstLetterName = 'M';
         if(firstLetterName == 'M') {
             System.out.println("ты случайно не Миша?");
         } else if(firstLetterName == 'I') {

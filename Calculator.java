22c2524972e36d94687c1b19d8958a2ab3d3148a
Yerzat Smagulov <smagulovyerzat@gmail.com>
2022-01-18 18:30:00 +0300

    Calculator ispravlen

--- Calculator.java
+++ Calculator.java
@@ -1,31 +1,24 @@
 public class Calculator {
     public static void main(String[] args) {
     int num1 = 2;
-    int num2 = 3;
-    int num3 = 1;
+    int num2 = 5;
     char operation = '^';
-    int result;
     if(operation == '+') {
-        result = num1 + num2;
-        System.out.println(result);
+        System.out.println(num1 + num2);
         } else if(operation == '-') {
-            result = num1 - num2;
-            System.out.println(result);
+            System.out.println(num1 - num2);
         } else if(operation == '*') {
-            result = num1 * num2;
-            System.out.println(result);
+            System.out.println(num1 * num2);
         } else if(operation == '/') {
-            result = num1 / num2;
-            System.out.println(result);
+            System.out.println(num1 / num2);
         } else if(operation == '^') {
-            for(int i=1; i <= num2; i++) {
+            int num3 = 1;
+            for(int i = 1; i <= num2; i++) {
                 num3 = num3 * num1;
             }
-            result = num3;
-            System.out.println(result);
+            System.out.println(num3);
         } else if(operation == '%') {
-            result = num1 % num2;
-            System.out.println(result);
+            System.out.println(num1 % num2);
         }
     }
 }

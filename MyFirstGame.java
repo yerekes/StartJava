c68636486f68ae56c888da30919572047016d53d
Yerzat Smagulov <smagulovyerzat@gmail.com>
2022-01-19 18:30:09 +0300

    Privet

--- MyFirstGame.java
+++ MyFirstGame.java
@@ -1,17 +1,21 @@
 public class MyFirstGame {
     public static void main(String[] args) {
-        int num = 99;
+        int num = 55;
         int random = 66;
+        if(num == random) {
+            System.out.println("Поздравляю, число угадано!");
+            } 
+
         while(num != random) {
             if(num > random) {
                 System.out.println("Данное число больше того, что загадал компьютер");
-                System.out.println("Введите число повторно");
                 break;
             } else if(num < random) {
                 System.out.println("Данное число меньше того, что загадал компьютер");
-                System.out.println("Введите число повторно");
+                break;
             } 
-            System.out.println("Поздравляю, число угадано!");
+            System.out.println("Введите число повторно");
         }
+
     }
 }
